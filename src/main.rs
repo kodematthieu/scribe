d835dcd48d92d2b9fd1d3610ac8a92df9bd3b1bb
Karel Matthieu L. Logro <kodematthieu@gmail.com>
2025-06-28 19:02:38 +0800

    feat(cli): Added clap CLI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,43 @@
+use std::{fs::File, io, path::PathBuf};
+
+use clap::Parser;
+use filetree::FileTree;
+use ignore::{WalkBuilder, overrides::OverrideBuilder};
+
 mod filetree;
 mod output;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Parser)]
+struct Command {
+    #[arg(default_value = ".")]
+    target: PathBuf,
+
+    #[arg(long, short, name = "FILE_NAME")]
+    output: Option<PathBuf>,
+}
+
+fn main() -> io::Result<()> {
+    let args = Command::parse();
+    let mut walker = WalkBuilder::new(&args.target);
+
+    if let Some(ref path) = args.output {
+        walker.overrides(OverrideBuilder::new(path).build().unwrap());
+    }
+
+    let tree = FileTree::new(&args.target, walker.build()).unwrap();
+
+    let mut writer: Box<dyn io::Write> = if let Some(path) = args.output {
+        Box::new(File::create(path)?)
+    } else {
+        Box::new(io::stdout())
+    };
+
+    let root_name = args.target.file_name().unwrap_or(args.target.as_os_str());
+    tree.display(root_name, &mut writer)?;
+
+    writeln!(writer, "\n---")?;
+
+    output::format(&args.target, tree, &mut writer)?;
+
+    Ok(())
 }
